b27720998f0607893bcda1436d758d5b030879d5
yunrap <dbsdpwl7@gmail.com>
2022-05-29 23:57:40 +0900

    함수표현식 기본구조

---
+++ ch4/code/Chapter4.js
@@ -0,0 +1,49 @@
+//4-2
+// var add = function (x, y) {
+//   return x + y;
+// };
+
+// var plus = add;
+
+// console.log(add(3, 5));
+// console.log(plus(1, 7));
+
+/****************************************/
+//4-3
+// var add = function sum(x, y) {
+//   return x + y;
+// };
+// console.log(add(3, 4));
+// console.log(sum(2, 6));
+
+/****************************************/
+//4-4
+// var factorialVar = function factorial(n) {
+//   if (n <= 1) {
+//     return 1;
+//   }
+//   return n * factorial(n - 1);
+// };
+
+// console.log(factorialVar(3));
+// console.log(factorial(3));  err.
+
+/****************************************/
+//4-6
+// console.log(add(3, 4));
+
+// function add(x, y) {
+//   return x + y;
+// }
+
+// console.log(add(3, 4));
+
+/****************************************/
+// 4-7
+//add(2, 3);    err.
+
+var add = function (x, y) {
+  return x + y;
+};
+
+add(3, 4);
