a3fdfc186b3b486323cd9ddefb0634a1063d352b
yunrap <dbsdpwl7@gmail.com>
2022-05-31 08:54:05 +0900

    함수 프로퍼티로사용가능

--- ch4/code/Chapter4.js
+++ ch4/code/Chapter4.js
@@ -47,3 +47,31 @@ var add = function (x, y) {
 };
 
 add(3, 4);
+
+/****************************************/
+// 4-8
+function add(x, y) {
+  return x + y;
+}
+
+add.result = add(3, 2);
+add.status = "OK";
+
+console.log(add.result); //5
+console.log(add.status); //OK
+
+/****************************************/
+// 4-9
+// 변수에 함수 할당
+var foo = 100;
+var bar = function () {
+  return 100;
+};
+console.log(bar());
+
+//프로퍼티에 함수 할당
+var obj = {};
+obj.baz = function () {
+  return 200;
+};
+console.log(obj.baz);
