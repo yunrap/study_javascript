251fc59ddd582afd45cb4533109d56ecf325f651
yunrap <dbsdpwl7@gmail.com>
2024-01-12 07:10:45 +0900

    document ready 스코프사용 개념정리

--- scopeChain/scopeEx.js
+++ scopeChain/scopeEx.js
@@ -7,3 +7,6 @@ if (true) {
   let y = 5; // ECMAScript 2015 도입 (블록문 스코프도입)
 }
 console.log(y);
+
+// 좋은예제
+// https://yuddomack.tistory.com/entry/%EC%9E%90%EB%B0%94%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8-%EC%8A%A4%EC%BD%94%ED%94%84scope
